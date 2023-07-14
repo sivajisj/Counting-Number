88b1ce983b48e260dd8483b3721a20b9d968241e
Sivaji <sivajigsivajig703@gmail.com>
2023-07-15 04:44:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react"
+
+
+export default function App() {
+    /**
+     * Challenge: 
+     * Add functionality to the minus button
+     */
+    const [count, setCount] = React.useState(0)
+    
+    function add() {
+        setCount(count + 1)
+    }
+    
+    function subtract() {
+        setCount(count - 1)
+    }
+    
+    return (
+        <div className="counter">
+        <div>
+        <h1 className="title">Counter</h1>
+        </div>
+            <button className="counter--minus" onClick={subtract}>–</button>
+            <div className="counter--count">
+                <h1>{count}</h1>
+            </div>
+            <button className="counter--plus" onClick={add}>+</button>
+        </div>
+    )
+}
